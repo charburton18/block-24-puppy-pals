2cd5552c742d35a788cbc97d7e44e092a33b6b10
Char B <charburton18@gmail.com>
2024-03-25 15:07:58 -0700

    got the puppyList to console log

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import './App.css'
+import puppyList from './data.js';
+// import './App.css'
 
 function App() {
+  console.log(puppyList);
 
   return (
     <>
       <div id="root"></div>
     </>
-  )
+  );
 }
 
 export default App
