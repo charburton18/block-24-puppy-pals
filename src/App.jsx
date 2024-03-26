74956ab2ffee9ab50b813be9ebd203d07b7c5e64
Char B <charburton18@gmail.com>
2024-03-26 09:48:34 -0700

    got puppies to display on page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
+import {useState} from 'react'
 import puppyList from './data.js';
 // import './App.css'
 
 function App() {
-  console.log(puppyList);
+  const [puppies, setPuppies] = useState(puppyList);
+  console.log("puppyList: ", puppies);
 
   return (
     <>
-      <div id="root"></div>
+      <div id="root" className="App">
+        {
+          puppies.map((puppy) => {
+            return <p key={puppy.id}>{puppy.name}</p>
+        })
+      }
+      </div>
     </>
   );
 }
 
 export default App
+
+//does it matter what order I import things in?
